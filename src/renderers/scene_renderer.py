9a8ec49cc5adbb95b85f5a9971333dd75b0450c9
wifi_mug <yaxding@gmail.com>
2023-06-30 17:19:36 +0100

    fix: re-added segmentation

--- src/renderers/scene_renderer.py
+++ src/renderers/scene_renderer.py
@@ -1,6 +1,7 @@
 import os
 import bpy
 
+from controllers.segmentation import Segmentation
 
 class SceneRenderer:
     def __init__(self, output_file, collection):
@@ -16,10 +17,10 @@ class SceneRenderer:
         bpy.context.scene.render.filepath = os.path.join(current_working_directory, self.output_file)
         bpy.ops.render.render(use_viewport=True, write_still=True)
 
-        # segmentation = Segmentation({
-        #     1: 0xffff, # Make cubes white in the segmentation map
-        #     2: 0xcccc,
-        #     3: 0x9999,
-        # })
-        # segmentation_filename = self.output_file.replace(".png", "_seg.png") if self.output_file.endswith(".png") else self.output_file + "_seg.png"
-        # segmentation.segment(segmentation_filename)
+        segmentation = Segmentation({
+            1: 0xffff, # Make cubes white in the segmentation map
+            2: 0xcccc,
+            3: 0x9999,
+        })
+        segmentation_filename = self.output_file.replace(".png", "_seg.png") if self.output_file.endswith(".png") else self.output_file + "_seg.png"
+        segmentation.segment(segmentation_filename)
