4cef635672a7335f80170c84f5995b74c3617ecf
wifi_mug <yaxding@gmail.com>
2023-07-05 00:44:30 +0100

    chore: fixed tests

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -40,28 +40,6 @@ class BlenderScriptTest(unittest.TestCase):
         # Verify that the output file was created
         self.assertTrue(os.path.isfile(self.expected_output_file))
 
-    # def test_cubes_created(self):
-    #     # Create test data YAML file
-    #     test_data = {
-    # #     "crop": {
-            #     "type": ['green','red','blue'],
-            #     "size": [0.5, 0.8, 1.0],
-            #     "percentage_share": [0.2, 0.3, 0.5],
-            #     "total_number": 9,
-            #     "num_rows": 2,
-            #     "row_widths": 5,
-            # }
-    #     }
-    #     with open(self.test_file, "w") as file:
-    #         yaml.safe_dump(test_data, file)
-    #
-    #     # Execute the script with simulated command-line arguments
-    #     run(["python", "src/launch.py", "-i", self.test_file, "-o", self.test_output])
-    #
-    #     # Verify that the correct number of cubes were created
-    #     collection = bpy.data.collections.get("Collection")
-    #     self.assertEqual(len(collection.objects), test_data["num_crops"])
-
     def test_render_output(self):
         # Create test data YAML file
         test_data ={
