aa7c0a6ef4357d05691a66dba3dd47d8b61ae38c
wifi_mug <yaxding@gmail.com>
2023-07-20 22:47:53 +0100

    style: fix linting issues

--- tests/controllers/test_light_controller.py
+++ tests/controllers/test_light_controller.py
@@ -1,6 +1,5 @@
 import unittest
 import bpy
-import mathutils
 from mathutils import Vector
 
 from src.controllers.light_controller import LightController
@@ -14,7 +13,7 @@ class LightControllerTest(unittest.TestCase):
         self.expected_sky_texture = "Sky Texture"
         self.initial_sky_type = "PREETHAM"
         self.changed_sky_type = "HOSEK_WILKIE"
-        self.changed_sun_direction = mathutils.Vector((1.0, 0.0, 1.0))
+        self.changed_sun_direction = (1.0, 0.0, 1.0)
 
     def test_add_light(self):
         light_controller = LightController()
@@ -24,13 +23,13 @@ class LightControllerTest(unittest.TestCase):
 
         # Verify that the output file was created
         self.assertEqual(get_position, self.expected_light_position)
-        
+
     def test_add_sky(self):
         light_controller = LightController()
         light_controller.add_sky()
         get_texture = bpy.context.scene.world.node_tree.nodes[self.expected_sky_texture]
         self.assertTrue(get_texture)
-        
+
     def test_change_sky_type(self):
         light_controller = LightController()
         get_sky_type = bpy.context.scene.world.node_tree.nodes[self.expected_sky_texture].sky_type
