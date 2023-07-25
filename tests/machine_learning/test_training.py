3cbc7c9db8843825f126136e4523052207a5ae11
Jiahao Shi <ucabhic@ucl.ac.uk>
2023-07-25 22:16:37 +0100

    style: improve style according to ruff

--- tests/machine_learning/test_training.py
+++ tests/machine_learning/test_training.py
@@ -1,18 +1,20 @@
 import unittest
 import numpy as np
 import shutil
+import cv2
+import os
 from pathlib import Path
-from src.machine_learning.utilities.data_preprocess import *
+from src.machine_learning.utilities.data_preprocess import extract_frames, slice_image, scale_image_to_fill
 
 class TrainingTest(unittest.TestCase):
 
 
     def test_extract_frames(self):
         # Test fail to open video file
-        with self.assertLogs('data_preprocess.extract_frames', level="ERROR") as cm:
+        with self.assertLogs("data_preprocess.extract_frames", level="ERROR") as cm:
             extract_frames("video_not_exist_path.mp4", "tmp/test_frames")
-        self.assertEqual(cm.output, ['ERROR:data_preprocess.extract_frames:Failed to open video file '
-                                     'video_not_exist_path.mp4'])
+        self.assertEqual(cm.output, ["ERROR:data_preprocess.extract_frames:Failed to open video file "
+                                     "video_not_exist_path.mp4"])
 
         # Create a random test video
         test_video_path = "tmp/test_video.mp4"
@@ -25,7 +27,7 @@ class TrainingTest(unittest.TestCase):
         duration = 2  # seconds
         frame_interval = 3 # extract every ... frames
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         video = cv2.VideoWriter(test_video_path, fourcc, frame_rate, (frame_width, frame_height))
 
         for _ in range(frame_rate * duration):
@@ -35,13 +37,13 @@ class TrainingTest(unittest.TestCase):
         video.release()
 
         # run the function
-        with self.assertLogs('data_preprocess.extract_frames', level="INFO") as cm:
+        with self.assertLogs("data_preprocess.extract_frames", level="INFO") as cm:
             extract_frames(test_video_path, test_frames_dir, frame_interval)
-        self.assertEqual(cm.output, [f'INFO:data_preprocess.extract_frames:Opened video file {test_video_path}',
-                                     f'INFO:data_preprocess.extract_frames:Finished extracting video {test_video_path}'])
+        self.assertEqual(cm.output, [f"INFO:data_preprocess.extract_frames:Opened video file {test_video_path}",
+                                     f"INFO:data_preprocess.extract_frames:Finished extracting video {test_video_path}"])
 
         # Check the output
-        frame_files = list(Path(test_frames_dir).glob('*'))
+        frame_files = list(Path(test_frames_dir).glob("*"))
         self.assertEqual(len(frame_files), duration * frame_rate // frame_interval + 1)
 
         # Delete the test video and frames
@@ -62,10 +64,10 @@ class TrainingTest(unittest.TestCase):
             self.assertEqual(chunk.shape, (512, 512, 3))
 
         # Test too small image
-        with self.assertLogs('data_preprocess.slice_image_square', level="WARNING") as cm:
+        with self.assertLogs("data_preprocess.slice_image_square", level="WARNING") as cm:
             slice_image(np.ones((500, 500, 3)))
-        self.assertEqual(cm.output, ['WARNING:data_preprocess.slice_image_square:Image not sliced: '
-                                     'original image size (500,500) is smaller than chunk size (512,512).'])
+        self.assertEqual(cm.output, ["WARNING:data_preprocess.slice_image_square:Image not sliced: "
+                                     "original image size (500,500) is smaller than chunk size (512,512)."])
 
     def test_scale_image_to_fill(self):
         image = scale_image_to_fill(np.ones((3840, 2160, 3)))
@@ -75,5 +77,5 @@ class TrainingTest(unittest.TestCase):
         self.assertEqual(image.shape, (512, 910, 3))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
