eed467fda159525783825583bde063fbd19bc072
taibohess <1738299580@qq.com>
2023-07-26 11:17:42 +0100

    fix: change api key to secret

--- src/controllers/weather_controller.py
+++ src/controllers/weather_controller.py
@@ -42,7 +42,3 @@ class WeatherController:
         return self.get_historical_weather(planting_date.strftime("%Y-%m-%d"), harvest_date.strftime("%Y-%m-%d"), lat, lon)
 
 
-api_key = "f9590b70bfae4938a98e0cbd86aa2877"
-weather_controller = WeatherController(api_key)
-weather_data = weather_controller.get_weather_for_growth_period("spring", "2023-02-01", 35.6895  , 139.6917  )
-print(weather_data)
