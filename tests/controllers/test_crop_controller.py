d8dd8ff64069eb0623c1658416f2a027999c0957
Danny <danielswarup1@gmail.com>
2023-08-09 00:15:01 +0100

    style: fix missing trailing comma

--- tests/controllers/test_crop_controller.py
+++ tests/controllers/test_crop_controller.py
@@ -20,7 +20,7 @@ class CropControllerTest(unittest.TestCase):
             "total_number": 9,
             "num_rows": 2,
             "row_widths": 5,
-            "density": 1
+            "density": 1,
         },
         "resolution": {
             "x": 512,
@@ -67,7 +67,7 @@ class CropControllerTest(unittest.TestCase):
                 "total_number": 10,
                 "num_rows": 2,
                 "row_widths": 5,
-                "density": 1
+                "density": 1,
             },
 
             "resolution": {
