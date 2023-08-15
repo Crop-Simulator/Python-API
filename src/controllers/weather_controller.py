0967202c309df98e863080384cff4f08339313ff
taibohess <1738299580@qq.com>
2023-08-15 08:03:39 +0200

    feat: get max temperature etc weather data

--- src/controllers/weather_controller.py
+++ src/controllers/weather_controller.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime, timedelta
+import os
 
 class WeatherController:
     HTTP_STATUS_OK = 200
@@ -8,26 +9,26 @@ class WeatherController:
         self.api_key = api_key
 
     def get_historical_weather(self, start_date, end_date, lat, lon):
-        base_url = "https://api.weatherbit.io/v2.0/history/daily"
-
+        base_url = "https://api.weather.com/v3/wx/hod/r1/direct"
         params = {
-            "key": self.api_key,
-            "start_date": start_date,
-            "end_date": end_date,
-            "lat": lat,
-            "lon": lon,
+            "apiKey": self.api_key,
+            "startDateTime": f"{start_date}T00Z",
+            "endDateTime": f"{end_date}T23Z",
+            "geocode": f"{lat},{lon}",
+            "format": "json",
+            "units": "e",
         }
+
         response = requests.get(base_url, params=params)
         if response.status_code == self.HTTP_STATUS_OK:
             data = response.json()
             processed_data = []
-            for day in data["data"]:
-                processed_data.append({
-                    "date": day["datetime"],
-                    "temperature": day["temp"],
-                    "precipitation": day["precip"],
-                    "humidity": day["rh"],
-                })
+            processed_data.append({
+                "date": data["validTimeUtc"],
+                "max_temperature": data["temperatureMax24Hour"],
+                "min_temperature": data["temperatureMin24Hour"],
+                "precipitation": data.get("precip1Hour", 0),
+            })
             return processed_data
         else:
             raise Exception(f"API request error with status code {response.status_code}")
@@ -40,4 +41,43 @@ class WeatherController:
             harvest_date = planting_date + timedelta(days=270)  # Approx. 9 months
         return self.get_historical_weather(planting_date.strftime("%Y-%m-%d"), harvest_date.strftime("%Y-%m-%d"), lat, lon)
 
+def extract_daily_data(self, data):
+    result = []
+
+    dates = data[0]["date"]
+    max_temps = data[0]["max_temperature"]
+    min_temps = data[0]["min_temperature"]
+    precipitations = data[0]["precipitation"]
+
+    current_day_data = {}
+
+    for i in range(len(dates)):
+        current_date = dates[i][:10]
+
+        if "date" not in current_day_data:
+            current_day_data["date"] = current_date
+            current_day_data["max_temperature"] = max_temps[i]
+            current_day_data["min_temperature"] = min_temps[i]
+            current_day_data["precipitation"] = precipitations[i]
+        elif current_date == current_day_data["date"]:
+            current_day_data["max_temperature"] = max(current_day_data["max_temperature"], max_temps[i])
+            current_day_data["min_temperature"] = min(current_day_data["min_temperature"], min_temps[i])
+            current_day_data["precipitation"] += precipitations[i]
+        else:
+            result.append(current_day_data.copy())
+            current_day_data["date"] = current_date
+            current_day_data["max_temperature"] = max_temps[i]
+            current_day_data["min_temperature"] = min_temps[i]
+            current_day_data["precipitation"] = precipitations[i]
+
+    result.append(current_day_data)
+    return result
+
+
+
+api_key = os.environ["WEATHER_API"]
+weather_controllerr = WeatherController(api_key)
+weather_data = weather_controllerr.get_weather_for_growth_period("spring", "2023-02-01", 35.6895, 149)
+daily_weather_data = weather_controllerr.extract_daily_data(weather_data)
+print(daily_weather_data)
 
