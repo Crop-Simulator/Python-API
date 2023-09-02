097b33af66cf5e25173e17e6392596b2cbf11763
Jiahao Shi <ucabhic@ucl.ac.uk>
2023-09-02 02:21:02 +0100

    feat: implement brush for weed annotation

--- src/machine_learning/utilities/interactive_annotator.py
+++ src/machine_learning/utilities/interactive_annotator.py
@@ -11,7 +11,7 @@ class ToolMode(Enum):
 
 # State variables for drawing
 drawing = False  # true if mouse is pressed
-tool_mode = ToolMode.ERASER
+tool_mode = ToolMode.BRUSH
 ix, iy = -1, -1
 
 # Variables for image display
@@ -90,7 +90,7 @@ trackbar_parameters = TrackbarParameters()
 
 # Mouse callback function
 def callback_draw_mask(event, x, y, flags, param):
-    global ix, iy, drawing, layer_weed
+    global ix, iy, drawing, layer_weed, flag_redo_merge_layers
 
     brush_size = cv2.getTrackbarPos('Brush Size', "Tools Window")
 
@@ -102,16 +102,20 @@ def callback_draw_mask(event, x, y, flags, param):
         if drawing:
             if tool_mode is ToolMode.BRUSH:
                 cv2.circle(layer_weed, (x, y), brush_size, 255, -1)
+                flag_redo_merge_layers = True
                 print(f"[Debug] {tool_mode} on ({x}, {y})")
 
             elif tool_mode is ToolMode.ERASER:
                 cv2.circle(layer_weed, (x, y), brush_size, 0, -1)
+                flag_redo_merge_layers = True
                 print(f"[Debug] {tool_mode} on ({x}, {y})")
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         if tool_mode is ToolMode.RECTANGLE:
             cv2.rectangle(layer_weed, (ix, iy), (x, y), 255, -1)
+            flag_redo_merge_layers = True
+            print(f"[Debug] {tool_mode} on ({ix}, {iy}) to ({x}, {y})")
 
 
 def extract_ground(image_hsv):
@@ -156,7 +160,7 @@ def interactive_annotator(image_path):
     target_window_width = cv2.getTrackbarPos("Disp Width", "Tools Window")
     cv2.resizeWindow("Images Display", target_window_width, int(target_window_width / image_aspect_ratio))
 
-    cv2.createTrackbar("Disp Mode", "Tools Window", 0, 2, trackbar_parameters.callback_display_mode)
+    cv2.createTrackbar("Disp Mode", "Tools Window", 0, 3, trackbar_parameters.callback_display_mode)
 
     # Trackbars for HSV thresholds
     cv2.createTrackbar('Lower H', "Tools Window", 35, 179, trackbar_parameters.callback_lower_h)
@@ -170,7 +174,7 @@ def interactive_annotator(image_path):
     cv2.createTrackbar('Smoothing', "Tools Window", 1, 30, trackbar_parameters.callback_closing_size)
 
     # Trackbars for drawing tools
-    cv2.createTrackbar('Brush Size', "Tools Window", 5, 50, trackbar_parameters.callback_brush_size)
+    cv2.createTrackbar('Brush Size', "Tools Window", 50, 300, trackbar_parameters.callback_brush_size)
 
     layer_ground = extract_ground(image_hsv)
     layer_merged_display_bgr = None
@@ -185,22 +189,35 @@ def interactive_annotator(image_path):
             # start with deep-copying an image
             layer_merged_display_bgr = image.copy()
 
-            # Find the pixels where the layer_ground is 0 (i.e., ground)
-            indices = np.where(layer_ground == 0)
+            # Find the pixels where the layer_ground is 0 (i.e., ground pixels)
+            ground_indices = np.where(layer_ground == 0)
+            # Find the pixels where the layer_weed is 255 (i.e., weed pixels)
+            weed_indices = np.where(layer_weed == 255)
 
             # Different display mode:
             if trackbar_parameters.display_mode == 0:
+                # Replace weed with red
+                layer_merged_display_bgr[weed_indices[0], weed_indices[1], :] = (100,100,220)
+
                 # Darken the ground area
-                layer_merged_display_bgr[indices[0], indices[1], :] = \
-                        layer_merged_display_bgr[indices[0], indices[1], :] // 3
+                layer_merged_display_bgr[ground_indices[0], ground_indices[1], :] = \
+                        image[ground_indices[0], ground_indices[1], :] // 3
 
             elif trackbar_parameters.display_mode == 1:
+                # Replace weed with red
+                layer_merged_display_bgr[weed_indices[0], weed_indices[1], :] = (100, 100, 220)
+
                 # replace ground with grey
-                layer_merged_display_bgr[indices[0], indices[1], :] = (127,127,127)
+                layer_merged_display_bgr[ground_indices[0], ground_indices[1], :] = (127,127,127)
 
             elif trackbar_parameters.display_mode == 2:
+                # Replace weed with black
+                layer_merged_display_bgr[weed_indices[0], weed_indices[1], :] = (0, 0, 0)
+
                 # replace ground with red
-                layer_merged_display_bgr[indices[0], indices[1], :] = (100,100,220)
+                layer_merged_display_bgr[ground_indices[0], ground_indices[1], :] = (100,100,220)
+
+            # do nothing when mode==3, i.e., show original image for reference
 
             flag_redo_merge_layers = False
 
